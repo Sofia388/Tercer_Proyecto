715fda475155807988abd60d8d824575cb5f9785
alecachin2002 <ale.ixtacuy2002@gmail.com>
2021-07-01 19:23:39 -0600

    base de datos

--- templates_c/597a5e0bcb4a828008590580f684dea265252e11_0.file.Inicio.tpl.php
+++ templates_c/597a5e0bcb4a828008590580f684dea265252e11_0.file.Inicio.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.38, created on 2021-06-23 20:22:31
+/* Smarty version 3.1.38, created on 2021-07-02 00:44:35
   from 'C:\wamp64\www\Tercer_Proyecto\View\Cabeceras\Inicio.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.38',
-  'unifunc' => 'content_60d398076ba175_68552787',
+  'unifunc' => 'content_60de6173102b40_29496754',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '597a5e0bcb4a828008590580f684dea265252e11' => 
     array (
       0 => 'C:\\wamp64\\www\\Tercer_Proyecto\\View\\Cabeceras\\Inicio.tpl',
-      1 => 1624479746,
+      1 => 1624489997,
       2 => 'file',
     ),
   ),
@@ -22,7 +22,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:Cabeceras/Footer.tpl' => 1,
   ),
 ),false)) {
-function content_60d398076ba175_68552787 (Smarty_Internal_Template $_smarty_tpl) {
+function content_60de6173102b40_29496754 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_subTemplateRender("file:Cabeceras/Header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
     
@@ -35,7 +35,7 @@ $_smarty_tpl->_subTemplateRender("file:Cabeceras/Header.tpl", $_smarty_tpl->cach
 
 
     <div class="row">
-    <form  method="post" action="?controller=User&method=BuscarUsuario" class="col s12">
+    <form  method="post" action="?controller=Usr&method=UsuarioExistente" class="col s12">
 
     <div>
     <h1 align="center">Login</h1>
@@ -50,7 +50,7 @@ $_smarty_tpl->_subTemplateRender("file:Cabeceras/Header.tpl", $_smarty_tpl->cach
 
       <div class="row" class="center-align">
          <div class="input-field col s5 s3 row offset-s2 center aling">
-             <input id="password" type="password" class="validate" required="" name="pass"/>
+             <input id="password" type="password" class="validate" required="" name="password"/>
              <label for="password">Contraseña</label>
             </div>
       </div>
